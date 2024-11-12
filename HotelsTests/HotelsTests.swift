4f91672f8ac92ba4060cc69305315fcebf26a104
Karina Kinzhigaliyeva <karinakinzhigaliyeva@gmail.com>
2024-11-12 23:11:39 +0500

    Initial Commit

---
+++ HotelsTests/HotelsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotelsTests.swift
+//  HotelsTests
+//
+//  Created by Karina Kinzhigaliyeva on 12.11.2024.
+//
+
+import Testing
+@testable import Hotels
+
+struct HotelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
